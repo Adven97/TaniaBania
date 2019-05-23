3f12199e2327c71c546aab122c9ee88afd808169
Adven97 <atomczak30@gmail.com>
2019-05-24 00:03:38 +0200

    dodana lista

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 const litr = document.querySelector('#wybor');
 const wolt = document.querySelector('#wolt');
 const cena = document.querySelector('#cena');
+let obl;
 
 wolt.addEventListener('keyup', numbersOnly);
 cena.addEventListener('keyup', numbersOnly);
@@ -16,12 +17,30 @@ licz.addEventListener('click', function(){
     }
 
     else{
-    const obl = (parseFloat(litr.value) * parseFloat(wolt.value)) / parseFloat(cena.value);
+    obl = (parseFloat(litr.value) * parseFloat(wolt.value)) / parseFloat(cena.value);
     document.querySelector('#wynik').textContent = obl;
     }
+    //showQuery();
+    addNewTrunek();
 });
 
 function numbersOnly() {
     let regex = /[a-z]/gi;
     this.value = this.value.replace(regex, "");
 }
+
+
+function addNewTrunek() {
+    const task = document.querySelector('input').value;
+
+    if (task.length > 0 && obl >0) {
+        const taskList = document.querySelector('.list-group');
+        let newTask = document.createElement('li');
+        newTask.setAttribute("class", "list-group-item");
+        let spanEl = document.createElement('span');
+        spanEl.appendChild(document.createTextNode(`Współczynnik taniej bani dla ${task} wynosi: ${obl}`));
+        newTask.appendChild(spanEl);
+        taskList.appendChild(newTask);
+    }
+}
+
